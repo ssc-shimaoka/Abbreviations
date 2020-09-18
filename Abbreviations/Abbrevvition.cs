8c75a2e4df611cf0b85a12129a0b8dbac4a97333
yuki shimaoka_home <yuki_shimaoka@solver.co.jp>
2020-09-18 11:52:04 +0900

    first commit

---
+++ Abbreviations/Abbrevvition.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abbreviations
+{
+    // 略語と対応する日本語を管理するクラス
+    class Abbreviaition
+    {
+        // 内部ディクショナリ
+        private Dictionary<string, string> _dict = new Dictionary<string, string>();
+
+        // コンストラクタ
+        public Abbreviaition()
+        {
+            var lines = File.ReadAllLines("Abbreviations.txt");
+
+            _dict = lines.Select(line => line.Split('='))
+                .ToDictionary(x => x[0], x => x[1]);
+        }
+
+        // 要素追加
+        public void Add(string abbr, string japanese)
+        {
+            _dict[abbr] = japanese;
+        }
+
+        // インデクサ
+        public string this[string abbr]
+        {
+            get
+            {
+                return _dict.ContainsKey(abbr) ? _dict[abbr] : null;
+            }
+        }
+
+
+        // 日本語から対応する省略語を取り出す
+        public string ToAbbreviation(string japanese)
+        {
+            return _dict.FirstOrDefault(x => x.Value == japanese).Key;
+        }
+
+
+        // 日本語 の 位置 を 引数 に 与え、 それ が 含ま れる 要素（ Key、 Value） を すべて 取り出す
+        public IEnumerable<KeyValuePair<string, string>> FindAll(string substring)
+        {
+            foreach(var item in _dict)
+            {
+                if(item.Value.Contains(substring))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
---
+++ Abbreviations/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abbreviations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // コンストラクタ呼び出し
+            var abbrs = new Abbreviaition();
+
+            // Add呼び出し
+            abbrs.Add("IOC", "国際オリンピック委員会");
+
+            // インデクサ利用
+            var names = new[] { "WHO", "FIFA", "NPT", };
+            foreach(var name in names)
+            {
+                var fullname = abbrs[name];
+                if(fullname == null)
+                {
+                    Console.WriteLine("{0}は見つかりません", name);
+                }
+                else
+                {
+                    Console.WriteLine("{0}={1}", name, fullname);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
